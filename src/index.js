226f1f4e812da1a95c8ef4a21cb6b96605249fbf
Andrei Alisenok <andreialisenok@gmail.com>
2023-01-02 17:43:32 +0300

    fix: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const tenths = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const hundredths = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 20) {
+        return tenths[number];
+    }
+    const n = number % 10;
+    if (number < 100) {
+        return (
+            hundredths[Math.floor(number / 10) - 2] +
+            (n ? " " + tenths[n] : "")
+        );
+    }
+
+    return (
+        tenths[Math.floor(number / 100)] +
+        " hundred" +
+        (number % 100 == 0 ? "" : " " + toReadable(number % 100))
+    );
+};
